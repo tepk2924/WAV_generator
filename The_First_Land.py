4478ed04d2f2149ea3b122889a70e74d0e6144fe
tepk2924 <149037487+tepk2924@users.noreply.github.com>
2023-10-26 13:49:19 +0900

    Add files via upload

---
+++ The_First_Land.py
@@ -0,0 +1,94 @@
+file = open("WAV_generator.py", mode = "r")
+code = file.read()
+file.close()
+code_splited = code.split("#----CODE_SEPARATION\n")
+exec(code_splited[0])
+
+# Parameters
+SAMPLING_RATE = 44100 #CONSTANT
+BASIC_FREQ = 440 #440Hz = A4, can be changed with "k", "f"
+VOLUME = 1 #100% at 1, 0% at 0, can be changed with "v"
+WAVE_TYPE = 0 #wave type(sine, square, etc), can be changed with "t"
+BPM = 220 #how many quarter notes in a single minute, can be changed with "b"
+TOTAL_TRACK_NUM = 6 #how many tracks are they, CONSTANT
+CURRENT_TRACK_NUM = 0 #track that you are adding note to, must be lower than TOTAL_TRACK_NUM, can be changed with "c"
+SIGMOID_NUMBER = 0 #min -0.8 @ input -1, 0 @ input 0, max 3 @ input 1; significantly slower if not 0, but able to change some tone of the sound, can be changed wit "S"
+INTERPOLATE_POINTS_X = [0,1/16,15/16,1]
+INTERPOLATE_POINTS_Y = [0,1,1,0] #those points are linearly interpolated, and the resulting function starting at 0 and ending at 1 is the filter applied each note. INT_X must be increasing sequence, 0 <= (each term in INT_Y) <= 1, can be changed with "i"
+TITLE = inspect.getfile(inspect.currentframe()).split("\\")[-1][0:-3]
+
+def stc(height, length_note, length_entire):
+    return [[height, length_note], ["r", length_entire - length_note]]
+
+main_chorus_A = ([[re, 1/4], [re, 1/4], [re, 1/6], [do, 1/12], [Lsib, 1/6], [re, 1/12]]
+        + [[Lsib, 1/4], [Lsib, 1/4], [Lsib, 1/6], [Lla, 1/12], [Lsol, 1/6], [Lsib, 1/12]]
+        + [[Lla, 1/4], [Lla, 1/6], [Lfa, 1/12],[Lla, 1/4], [Lla, 1/6], [Lfa, 1/12]]
+        + [[Lsol, 1/4], [Lsol, 1/6], [Lmi, 1/12], [Lsol, 1/6], [Lla, 1/12], [do, 1/6], [mi, 1/12]]) * 2
+main_chorus_B = ([[re, 1/4], [re, 1/6], [mi, 1/12], [fa, 1/4], [re, 1/4]]
+        + [[Lsib, 1/6], [Lsol, 1/12], [Lsib, 1/6], [Lsol, 1/12], [fa, 1/6], [sol, 1/12], [la, 1/6], [fa, 1/12]]
+        + [[Lsib, 1/4], [Lsib, 1/6], [Lsib, 1/12], [fa, 1/6], [re, 1/12], [Lla, 1/6], [Lfa, 1/12]]
+        + [[Lsol, 1/6], [Lmi, 1/12], [Lsol, 1/6], [Lmi, 1/12], [Lsol, 1/6], [Lsol, 1/12], [Lsol, 1/6], [Lla, 1/12]]) * 2
+percusion_initial = [["v", 0.05], [0, 1/24], ["r", 5/24], ["v", 0.2], [0, 1/24], ["r", 1/8], ["v", 0.05], [0, 1/24], ["r", 1/24]] * 16
+percusion_A = [["v", 0.05], [0, 1/4], ["r", 1/4], ["v", 0.2], [0, 1/4], ["v", 0.05], [0, 1/4]] * 8
+percusion_B = [["v", 0.05], [0, 1/4], [0, 1/6], [0, 1/12], ["v", 0.2], [0, 1/4], ["v", 0.05], [0, 1/4]] * 8
+base_A = [[LLre, 1/2], [Lre, 1/4], [LLre, 1/4], [LLLsib, 1/2], [LLsib, 1/4], [LLLsib, 1/4], [LLfa, 1/2], [Lfa, 1/4], [LLfa, 1/4], [LLdo, 1/2], [Ldo, 1/4], [LLdo, 1/4]] * 2
+base_B = [[LLre, 1/4], [Lre, 1/4], [Lre, 1/4], [LLre, 1/4], [LLLsib, 1/4], [LLsib, 1/4], [LLsib, 1/4], [LLLsib, 1/4], [LLfa, 1/4], [Lfa, 1/4], [Lfa, 1/4], [LLfa, 1/4], [LLdo, 1/4], [Ldo, 1/4], [Ldo, 1/4], [LLdo, 1/4]] * 2
+beep1 = [["r", 1/2], [Hfa, 15/32], ["r", 17/32], [Hre, 15/32], ["r", 17/32], [Hsib, 15/32], ["r", 17/32], [HHdos, 15/32], ["r", 1/32]] * 2
+beep2 = [["r", 17/32], [fa, 15/32], ["r", 17/32], [re, 15/32], ["r", 17/32], [sib, 15/32], ["r", 17/32], [Hdos, 15/32]] * 2
+
+notes += [["i", [0,0], [1/16, 0.4], [1, 0.4]], ["S", 1], ["t", 1], ["v", 0.15]] #track 0, main chorus, square wave, volume 0.15
+notes += (stc(re, 1/12, 1/4) + stc(re, 1/12, 1/4) + stc(re, 1/12, 1/6) + stc(do, 1/12, 1/12) + stc(Lsib, 1/12, 1/6) + stc(re, 1/12, 1/12)
+        + stc(Lsib, 1/12, 1/4) + stc(Lsib, 1/12, 1/4) + stc(Lsib, 1/12, 1/6) + stc(Lla, 1/12, 1/12) + stc(Lsol, 1/12, 1/6) + stc(Lsib, 1/12, 1/12)
+        + stc(Lla, 1/12, 1/4) + stc(Lla, 1/12, 1/6) + stc(Lfa, 1/12, 1/12) + stc(Lla, 1/12, 1/4) + stc(Lla, 1/12, 1/6) + stc(Lfa, 1/12, 1/12)
+        + stc(Lsol, 1/12, 1/4) + stc(Lsol, 1/12, 1/6) + stc(Lmi, 1/12, 1/12) + stc(Lsol, 1/12, 1/6) + stc(Lla, 1/12, 1/12) + stc(do, 1/12, 1/6) + stc(mi, 1/12, 1/12)) * 2
+notes += [["s", 0],["S", .5], ["i", [0,0], [1/32, 1], [1,.3]]]
+notes += main_chorus_A
+notes += [["s", 1]]
+notes += main_chorus_B
+notes += [["s", 2]]
+notes += main_chorus_A
+notes += [["s", 3]]
+notes += main_chorus_B
+
+notes += [["c", 1], ["i", [0, 0], [1/16, 1], [1, 1]], ["S", 0], ["t", 4], ["v", 0.05]] #track 1, percusion, white noise, volume 0.05
+notes += percusion_initial
+notes += [["l", 0], ["i", [0, 0], [1/32, 1], [1, 0]]]
+notes += percusion_A
+notes += [["l", 1]]
+notes += percusion_B
+notes += [["l", 2]]
+notes += percusion_A
+notes += [["l", 3]]
+notes += percusion_B
+
+notes += [["c", 2], ["i", [0, 0], [1/16, 1], [1, 0]], ["S", -1], ["t", 6], ["v", 0.15]] #track 2, base, wave_type 6(S=-1), volume 0.15
+notes += [["l", 0]]
+notes += base_A
+notes += [["l", 1]]
+notes += base_B
+notes += [["l", 2]]
+notes += base_A
+notes += [["l", 3]]
+notes += base_B
+
+notes += [["c", 3], ["i", [0, 1], [1/16, 1], [2/16, 0], [3/16, 0.8], [4/16, 0], [5/16, 0.8**2], [6/16, 0], [7/16, 0.8**3], [8/16, 0], [9/16, 0.8**4], [10/16, 0.8**5], [11/16, 0], [1, 0]], ["S", 0], ["t", 1], ["v", 0.15]] #track 3, beep1, square, volume 0.15
+notes += [["l", 0]]
+notes += beep1
+notes += [["l", 1]]
+notes += beep1
+notes += [["l", 2]]
+notes += beep1
+notes += [["l", 3]]
+notes += beep1
+
+notes += [["c", 4], ["i", [0, 1], [1/16, 1], [2/16, 0], [3/16, 0.8], [4/16, 0], [5/16, 0.8**2], [6/16, 0], [7/16, 0.8**3], [8/16, 0], [9/16, 0.8**4], [10/16, 0.8**5], [11/16, 0], [1, 0]], ["S", 0], ["t", 1], ["v", 0.15]] #track 4, beep2, square, volume 0.15
+notes += [["l", 0]]
+notes += beep2
+notes += [["l", 1]]
+notes += beep2
+notes += [["l", 2]]
+notes += beep2
+notes += [["l", 3]]
+notes += beep2
+
+exec(code_splited[1])
---
+++ WAV_generator.py
@@ -0,0 +1,187 @@
+import numpy as np
+import time
+from numpy import interp
+from numpy import concatenate
+from numpy import linspace
+from numpy import where
+from scipy.io.wavfile import write
+import os, inspect
+
+start_time = time.time()
+
+def wave_function(height, t):
+    a = (BASIC_FREQ * 2**(height/12) * t) % 1
+    x = where(SIGMOID_NUMBER == 0, a, where(a != 0, 1 / (1 + ((1 - a)/a)**(SIGMOID_NUMBER + 1)), 0))
+    if WAVE_TYPE == 0: #sine_wave
+        result_wave = np.sin(2 * np.pi * x)
+    elif WAVE_TYPE == 1: #square_wave
+        result_wave = where(x <= 1/4, -1, where(x <= 3/4, 1, -1))
+    elif WAVE_TYPE == 2: #sawtooth wave
+        result_wave = 2*x - 1
+    elif WAVE_TYPE == 3: #triangular wave(similar to sine_wave)
+        result_wave = 2 * np.abs(2*x - 1) - 1
+    elif WAVE_TYPE == 4: #white noise, note[0] does nothing in frequency
+        result_wave = np.random.normal(0, 1, len(t))
+    elif WAVE_TYPE == 5: #white noise filtered with sine wave. percusion with some sound height
+        result_wave = np.sin(2 * np.pi * x) * np.random.normal(0, 1, len(t))
+    elif WAVE_TYPE == 6: #some husky sound, and with S = -1 generate electric-themed sound
+        result_wave = where(x < 1/6, -0.5+6*x, where(x < 2/6, 0.5 - 9*(x-1/6), where(x < 3/6, 1 - 9*(x-2/6), where(x < 4/6, 0.75-9*(x-3/6), where(x < 5/6, 0.75-9*(x-4/6), -0.75+1.5*(x-5/6))))))
+    return result_wave
+
+class track:
+    count = 0
+    def __init__(self):
+        self.id = track.count
+        self.wave = np.array([])
+        self.length = 0
+        track.count += 1
+
+    def concatenate(self, height, t, note_duration, note_length):
+        self.wave = concatenate((self.wave, VOLUME * wave_function(height, t) * interp(t / note_duration, INTERPOLATE_POINTS_X, INTERPOLATE_POINTS_Y)))
+        self.length += note_length
+
+    def ornament_note(self, height_ornament, height_main, t, ornament_ratio, note_duration, note_length):
+        self.wave = concatenate((self.wave, VOLUME * (wave_function(height_ornament, t) * where(t / note_duration < ornament_ratio, 1, 0)
+                                                    + wave_function(height_main, t)     * where(t / note_duration < ornament_ratio, 0, 1))
+                                    * interp(t / note_duration, INTERPOLATE_POINTS_X, INTERPOLATE_POINTS_Y)))
+        self.length += note_length
+
+    def padding(self, location):
+        if location > self.length:
+            self.wave = np.pad(self.wave, (0, location - self.length), mode = "constant")
+            self.length = location
+
+    def restnote(self, restnote_length):
+        self.wave = concatenate((self.wave, np.zeros(restnote_length)))
+        self.length += restnote_length
+
+str_to_height = {"do" : -9, "do#" : -8, "dos" : -8, "reb" : -8,
+                 "re" : -7, "re#" : -6, "res" : -6, "mib" : -6,
+                 "mi" : -5, "fa" : -4, "fa#" : -3, "fas" : -3,
+                 "solb" : -3, "sol" : -2, "sol#" : -1, "sols" : -1,
+                 "lab" : -1, "la" : 0, "la#" : 1, "las" : 1, "sib" : 1, "si" : 2}
+oct_change = {"H" : 12, "L" : -12}
+
+LLLdo, LLLdos, LLLreb, LLLre, LLLres, LLLmib, LLLmi, LLLfa, LLLfas, LLLsolb, LLLsol, LLLsols, LLLlab, LLLla, LLLlas, LLLsib, LLLsi = -45, -44, -44, -43, -42, -42, -41, -40, -39, -39, -38, -37, -37, -36, -35, -35, -34
+LLdo , LLdos , LLreb , LLre , LLres , LLmib , LLmi , LLfa , LLfas , LLsolb , LLsol , LLsols , LLlab , LLla , LLlas , LLsib , LLsi  = -33, -32, -32, -31, -30, -30, -29, -28, -27, -27, -26, -25, -25, -24, -23, -23, -22
+Ldo  , Ldos  , Lreb  , Lre  , Lres  , Lmib  , Lmi  , Lfa  , Lfas  , Lsolb  , Lsol  , Lsols  , Llab  , Lla  , Llas  , Lsib  , Lsi   = -21, -20, -20, -19, -18, -18, -17, -16, -15, -15, -14, -13, -13, -12, -11, -11, -10
+do   , dos   , reb   , re   , res   , mib   , mi   , fa   , fas   , solb   , sol   , sols   , lab   , la   , las   , sib   , si    =  -9,  -8,  -8,  -7,  -6,  -6,  -5,  -4,  -3,  -3,  -2,  -1,  -1,   0,   1,   1,   2
+Hdo  , Hdos  , Hreb  , Hre  , Hres  , Hmib  , Hmi  , Hfa  , Hfas  , Hsolb  , Hsol  , Hsols  , Hlab  , Hla  , Hlas  , Hsib  , Hsi   =   3,   4,   4,   5,   6,   6,   7,   8,   9,   9,  10,  11,  11,  12,  13,  13,  14
+HHdo , HHdos , HHreb , HHre , HHres , HHmib , HHmi , HHfa , HHfas , HHsolb , HHsol , HHsols , HHlab , HHla , HHlas , HHsib , HHsi  =  15,  16,  16,  17,  18,  18,  19,  20,  21,  21,  22,  23,  23,  24,  25,  25,  26
+notes = []
+
+#----CODE_SEPARATION
+
+tracks = []
+for i in range(TOTAL_TRACK_NUM):
+    tracks.append(track())
+
+locations = np.zeros(256, dtype = int)
+
+iterations = 0
+total_notes = len(notes)
+DIV_NUM = 10
+milestone = set([])
+for i in range(DIV_NUM):
+    milestone.add(int(np.around(total_notes * i / DIV_NUM)))
+
+for note in notes:
+    iterations += 1
+    if str(type(note[0])) == "<class 'int'>" or str(type(note[0])) == "<class 'float'>": #note[0] is height; note[1] is length
+        note_duration = 240 * note[1] / BPM
+        note_length = int(note_duration * SAMPLING_RATE)
+        t = linspace(0, note_duration, note_length ,endpoint=False)
+        height = note[0]
+        tracks[CURRENT_TRACK_NUM].concatenate(height, t, note_duration, note_length)
+    elif len(note[0]) >= 2: #note[0] is height_str; note[1] is length; note[2] is octave (void is 0); height_str(note[0]) is "do", "re", "mi" and so on, prefixed with "H" to notate higher octave and "L" to lower octave, "la" notates BASIC_FREQ
+        note_duration = 240 * note[1] / BPM
+        note_length = int(note_duration * SAMPLING_RATE)
+        t = linspace(0, note_duration, note_length ,endpoint=False)
+        height_str = note[0]
+        height = 0
+        while (height_str[0] == "H" or height_str[0] == "L"):
+            height += oct_change[height_str[0]]
+            height_str = height_str[1:]
+        height += str_to_height[height_str]
+        tracks[CURRENT_TRACK_NUM].concatenate(height, t, note_duration, note_length)
+    elif note[0] == "o": #note with ornament, ["o", "re", 1/16, "mi", 1/4] is note with length of the quarter note with ornament length is 1/16 of the length of the note (1/4 * 1/16 = 64th note)
+        note_duration = 240 * note[4] / BPM
+        note_length = int(note_duration * SAMPLING_RATE)
+        t = linspace(0, note_duration, note_length,endpoint=False)
+
+        if str(type(note[1])) == "<class 'int'>" or str(type(note[1])) == "<class 'float'>":
+            height_ornament = note[1]
+        else:
+            height_str = note[1]
+            height_ornament = 0
+            while (height_str[0] == "H" or height_str[0] == "L"):
+                height_ornament += oct_change[height_str[0]]
+                height_str = height_str[1:]
+            height_ornament += str_to_height[height_str] 
+
+        if str(type(note[3])) == "<class 'int'>" or str(type(note[3])) == "<class 'float'>":
+            height_main = note[3]
+        else:
+            height_str = note[3]
+            height_main = 0
+            while (height_str[0] == "H" or height_str[0] == "L"):
+                height_main += oct_change[height_str[0]]
+                height_str = height_str[1:]
+            height_main += str_to_height[height_str]   
+        tracks[CURRENT_TRACK_NUM].ornament_note(height_ornament, height_main, t, note[2], note_duration, note_length)
+    elif note[0] == "b": #BPM change by const(note[1])
+        BPM = note[1]
+    elif note[0] == "k": #BASIC_FREQ change by KEY
+        BASIC_FREQ *= 2**(note[1]/12)
+    elif note[0] == "f": #BASIC_FREQ change by freqency constant in Hz
+        BASIC_FREQ = note[1]
+    elif note[0] == "r": #rest note, does not sound
+        note_duration = 240 * note[1] / BPM
+        tracks[CURRENT_TRACK_NUM].restnote(int(note_duration * SAMPLING_RATE))
+    elif note[0] == "v": #change volume; note[1] is volume, 100% is 1, 0% is 0
+        VOLUME = note[1]
+    elif note[0] == "t": #change wave_type
+        WAVE_TYPE = note[1]
+    elif note[0] == "c": #change CURRENT_TRACK_NUM
+        CURRENT_TRACK_NUM = note[1]
+    elif note[0] == "s": #save current length of current track, note[1] is location
+        locations[note[1]] = tracks[CURRENT_TRACK_NUM].length
+    elif note[0] == "l": #load location from locations and pad to current location
+        tracks[CURRENT_TRACK_NUM].padding(locations[note[1]])
+    elif note[0] == "i": #["i", [0, 0], [1/16, 1], [15/16, 1], [1, 0]] is default setting
+        point_num = len(note) - 1
+        INTERPOLATE_POINTS_X = []
+        INTERPOLATE_POINTS_Y = []
+        for i in range(point_num):
+            INTERPOLATE_POINTS_X.append(note[i+1][0])
+            INTERPOLATE_POINTS_Y.append(note[i+1][1])
+    elif note[0] == "S": #change SIGMOID_NUMBER by note[1]
+        if note[1] < 0:
+            SIGMOID_NUMBER = 0.8 * note[1]
+        elif note[1] > 0:
+            SIGMOID_NUMBER = 3 * note[1]
+        else:
+            SIGMOID_NUMBER = 0
+
+
+    if iterations in milestone:
+        print(str(iterations) + " out of " + str(total_notes) + " notes done (" + str(int(np.around(100 * iterations/total_notes))) + "%), " + str(time.time() - start_time) + " sec elapsed")
+
+max_length = 0
+for i in range(TOTAL_TRACK_NUM):
+    if tracks[i].length > max_length:
+        max_length = tracks[i].length
+
+for i in range(TOTAL_TRACK_NUM):
+    tracks[i].padding(max_length)
+
+merged_wave = np.zeros(max_length)
+for i in range(TOTAL_TRACK_NUM):
+    merged_wave += tracks[i].wave
+
+# Convert to 16-bit PCM format, result: lowest val = -32768 @ t = 0, 1/f, 2/f, ..., highest val = 32767 @ t = 0.5/f, 1.5/f, 2.5/f, ... 
+scaled_wave = np.int16(merged_wave * 32767)
+
+# Save the WAV file & close the file
+write(TITLE + ".wav", SAMPLING_RATE, scaled_wave)
+print("done! (100%), " + str(time.time() - start_time) + " sec elapsed")
